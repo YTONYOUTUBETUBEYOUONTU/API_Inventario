b36dfd426d13c8bd516e0495b21e9a9153c5daf6
YTONYOUTUBETUBEYOUONTU <hu210111050@lahuerta.tecmm.edu.mx>
2025-05-30 22:11:03 -0600

    update-0.5

--- index.js
+++ index.js
@@ -5,11 +5,25 @@ const { PrismaClient } = require('./generated/prisma');
 const app = express();
 const prisma = new PrismaClient();
 
+const allowedOrigins = [
+  'http://localhost:5173',
+  'capacitor://localhost',
+  'https://localhost',
+];
+
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: function(origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = `El CORS policy no permite el origen: ${origin}`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
+  credentials: true,
 }));
+
 app.use(express.json());
 
 app.get('/usuarios', async (req, res) => {
